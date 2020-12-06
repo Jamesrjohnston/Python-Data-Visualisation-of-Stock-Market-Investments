61a495fd74e6c581965301110b1f0f65178ce6e5
James Johnston <James.johnston@iinet.net.au>
2020-12-06 17:25:11 +0800

    pd.read_csv
    laoding in stake file into a dataframe

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from pandas import Series, DataFrame
+
+import matplotlib 
+
+df = pd.read_csv(r'C:\Users\Johnston\Documents\Projects\Python-Data-Visualisation-of-Stock-Market-Investments\StakeFile1.csv')
+
+print(df)
